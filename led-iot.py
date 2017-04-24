1315d8d2834c53e09016a6c0414e0e05256a07ab
Luis <luis.chavez@tektonlabs.com>
2017-04-24 21:11:11 +0000

    initial commit

--- led-iot.py
+++ led-iot.py
@@ -6,9 +6,9 @@ import pyrebase
 #Firebase Configuration
 config = {
   "apiKey": "apiKey",
-  "authDomain": "[YOUR PROJECT ID].firebaseapp.com",
-  "databaseURL": "[YOUR PROJECT ID].firebaseio.com",
-  "storageBucket": "[YOUR PROJECT ID].appspot.com"
+  "authDomain": "demoiot-6579b.firebaseapp.com",
+  "databaseURL": "https://demoiot-6579b.firebaseio.com",
+  "storageBucket": "demoiot-6579b.appspot.com"
 }
 
 firebase = pyrebase.initialize_app(config)
@@ -17,15 +17,28 @@ firebase = pyrebase.initialize_app(config)
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(18, GPIO.OUT)
+GPIO.setup(23, GPIO.IN, GPIO.PUD_DOWN)
 
 #Firebase Database Intialization
 db = firebase.database()
 
 #While loop to run until user kills program
 while(True):
+    inputValue = GPIO.input(23)
+    if (inputValue == True):
+       print("button pressed")
+       led = db.child("led").get()
+       
+       for user in led.each():
+           if(user.val() == "OFF"):
+              data = {'state':'ON'}
+              db.child("led").update(data)               
+           else: 
+              data = {'state':'OFF'}
+              db.child("led").update(data)
+   	
     #Get value of LED 
     led = db.child("led").get()
-
     #Sort through children of LED(we only have one)
     for user in led.each():
         #Check value of child(which is 'state')
@@ -39,3 +52,4 @@ while(True):
         #0.1 Second Delay
         time.sleep(0.1)   
 
+	
